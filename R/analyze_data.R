47291807543f9068da9cbfb3c561653fcc3fd562
Daniel Krüger <daniel.krueger@gfk.com>
2016-10-29 17:45:38 +0200

    initialisierunf funktion

---
+++ R/analyze_data.R
@@ -0,0 +1,96 @@
+#' analyze_data
+#'
+#' @param inp input data.frame as provided by import data
+#' @param time_col col.name of inp which contains temporal data
+#' @param pattern used to subset the data, multiple input separated by "|"
+#' @param pattern_col column to pattern should be applied
+#'
+#' @return dataframe with aggregated data
+#' @export
+#'
+#' @examples
+#' dd <- import_data()
+#'
+#' #' Example 1: Defaults --------------------------------------------------
+#' dd2 <- analyze_data(dd)
+#'
+#' ggplot(dd2, aes(x = time, y = value, group = variable, col = variable),
+#'        alpha = .6) +
+#'   geom_line(lwd = 1) +
+#'   geom_point(size = 3) +
+#'   theme_dark() +
+#'   theme(axis.text.x = element_text(angle = 90, hjust = .5))
+#'
+#'
+#' #' Example 2: Lohn --------------------------------------------------
+#'
+#' #'remove first + last month from data
+#' dd_ <- subset(dd,!month %in% range(dd$month))
+#'
+#' dd2 <- analyze_data(dd_,pattern = "gfk|fraunhofer", time_col = "month")
+#'
+#'
+#' ggplot(dd2, aes(x = time, y = value, group = variable, col = variable),
+#'        alpha = .6) +
+#'   geom_line(lwd = 1) +
+#'   geom_point(size = 3) +
+#'   geom_smooth(method = "lm", lwd = .5, lty = "dashed") +
+#'   theme_dark() +
+#'   theme(axis.text.x = element_text(angle = 90, hjust = .5))
+#'
+#'
+#' #' Example 3: Vodafone -----------------------------------------------
+#' dd2 <- analyze_data(dd_,pattern = "vodafone", time_col = "month")
+#'
+#'
+#' ggplot(dd2, aes(x = time, y = value, group = variable, col = variable),
+#'        alpha = .6) +
+#'   geom_line(lwd = 1) +
+#'   geom_point(size = 3) +
+#'   geom_smooth(method = "lm", lwd = .5, lty = "dashed") +
+#'   theme_dark() +
+#'   theme(axis.text.x = element_text(angle = 90, hjust = .5))
+#'
+#'
+#' #' Example 4: Hausgeld ---------------------------------------------
+#' dd2 <- analyze_data(dd_,pattern = "FRIEDENSTRASSE", time_col = "month")
+#'
+#'
+#' ggplot(dd2, aes(x = time, y = value, group = variable, col = variable),
+#'        alpha = .6) +
+#'   geom_line(lwd = 1) +
+#'   geom_point(size = 3) +
+#'   geom_smooth(method = "lm", lwd = .5, lty = "dashed") +
+#'   theme_dark() +
+#'   theme(axis.text.x = element_text(angle = 90, hjust = .5))
+analyze_data <- function(inp, time_col = "week", pattern = "aldi|rewe|edeka", pattern_col = "AuftraggeberBegünstigter") {
+  dd <- subset(inp, grepl(pattern = tolower(pattern), x = tolower(inp[, pattern_col])))
+  cat(round(100 * nrow(dd) / nrow(inp), 1), " % of all movements match pattern\n")
+
+  dd2 <- summarise(group_by_(dd, pattern_col, time_col),
+                   Betrag = sum(Betrag, na.rm = T))
+  dd2 <- as.data.frame(dd2)
+  all_pat <- unlist(stringr::str_split(string = pattern, pattern = "\\|"))
+  #
+  # comp_amount <- function(tt = dd2, pat, pattern_col = pattern_col, col_ = "Betrag") {
+  #   tt <- as.data.frame((tt))
+  #   tt[, pat] <- ifelse(grepl(pattern = tolower(pat), x = tolower(tt[, pattern_col])),
+  #                       1, 0)
+  #   return(tt)
+  # }
+
+  for (pat_ in all_pat) {
+    dd2[, pat_] <- ifelse(grepl(pattern = tolower(pat_), x = tolower(dd2[, pattern_col])),
+                          1, 0) * dd2$Betrag
+  }
+
+  dd3 <- reshape2::melt(data = dd2, id.vars = time_col, measure.vars = all_pat)
+  names(dd3)[names(dd3) == time_col] <- "time"
+
+  dd4 <- as.data.frame(summarise(group_by(dd3, time, variable),
+                                 value = sum(value)
+  ))
+
+  return(dd4)
+
+}
---
+++ R/import_data.R
@@ -0,0 +1,87 @@
+#' import_data
+#'
+#' @param last_file character file giving path to csv-input
+#'
+#' @return data.frame with imported and cleaned data
+#'
+#' @details if last_file is not provided, the last downloaded file is automatically chosen
+#'
+#' @export
+#'
+#' @examples
+#' import_data()
+import_data <- function(last_file = get_last_file()) {
+  dd <- read.csv(last_file, skip = 8, sep = ";", stringsAsFactors = F)
+  dim(dd)
+  names(dd) <- c("Buchungstag",	"Wertstellung", "Buchungstext",	"AuftraggeberBegünstigter",
+                 "Verwendungszweck",	"Kontonummer",	"BLZ",
+                 "Betrag",	"Gläubiger-ID",	"Mandatsreferenz", 	"Kundenreferenz",
+                 "12")
+  dd$Betrag <- as.numeric(gsub(",", ".", gsub("\\.", "", dd$Betrag)))
+
+  dd$day <- as.Date(dd$Buchungstag, format = "%d.%m.%Y")
+  dd$week <- paste0(strftime(dd$day,format="%Y"), strftime(dd$day,format="%W"))
+  dd$month <- paste0(strftime(dd$day,format="%Y"), strftime(dd$day,format="%m"))
+
+  cat("\n\n# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #\n",
+      "Import summary:-----------------------------------------------------\n",
+      "account movements: ", nrow(dd), "\n",
+      "max deposit: ", max(dd$Betrag, na.rm = T), " - ", substr(dd$Verwendungszweck[which.max(dd$Betrag)], 1, 30), "\n",
+      "max payout: ", min(dd$Betrag, na.rm = T), " - ", substr(dd$Verwendungszweck[which.min(dd$Betrag)], 1, 30), "\n",
+      "time range", as.character(range(dd$day)))
+
+  return(dd)
+
+}
+
+
+# --------------------------------
+
+get_ff_profile_dir <- function() {
+  user_name <- shell(cmd = "echo %USERNAME%", intern = T)
+  bdir <- paste0("C:/Users/", user_name, "/AppData/Roaming/Mozilla/Firefox/")
+
+  cat("Hello", user_name, "\nGetting your ff profile directory")
+  tmp <- readLines(paste0(bdir, "profiles.ini"))
+  tmp <- grep("Path\\=Profiles\\/", tmp, value = T)
+  tmp <- gsub("Path\\=Profiles\\/", "", tmp)
+
+  ff_dir <- paste0(bdir, "Profiles/", tmp)
+  if (!dir.exists(ff_dir)) {stop("couldnt finf ff profile dir")
+  } else {return(ff_dir)}
+
+}
+
+# read last downloaded file
+get_last_downloaded_filename <- function(ff_dir = get_ff_profile_dir()) {
+
+  dbname <- paste0(ff_dir, "/places.sqlite")
+  con = dbConnect(RSQLite::SQLite(), dbname=dbname)
+  ii <- 2
+  alltables <- dbListTables(con)
+  tab <- alltables[ii]
+  # get the populationtable as a data.frame
+  p1 <- dbGetQuery(con, paste0('select * from ', tab))
+  head(p1)
+
+  last_file <- tail(grep("\\.csv", p1$content, value = T), 1)
+}
+
+get_download_dir <- function(ff_dir = get_ff_profile_dir()) {
+  js_file <- paste0(ff_dir, "/prefs.js")
+
+  tmp <- readLines(js_file)
+  tmp <- grep("browser\\.download\\.dir", tmp, value = T)
+  tmp <- stringr::str_split(string = tmp, pattern = "browser\\.download\\.dir")[[1]][2]
+
+  regexpr(pattern = '\",', text = tmp)
+  tmp <- gsub('");', "", gsub(pattern = '\", \"', "", tmp))
+  return(tmp)
+}
+
+
+get_last_file <- function() {
+  last_file <- paste(get_download_dir(), get_last_downloaded_filename(), sep="\\\\")
+  file.exists(last_file)
+  return(last_file)
+}
